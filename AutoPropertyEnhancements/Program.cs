cb38b6bb89fc26cd7ea31b9d32ce85ea36a4f1f8
csyntax <csyntax@outlook.com>
2016-05-21 09:42:47 +0300

    add AutoPropertyEnhancements demo

---
+++ AutoPropertyEnhancements/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace AutoPropertyEnhancements
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Student simplyStudent = new Student();
+
+            Console.WriteLine(simplyStudent);
+        }
+    }
+}
---
+++ AutoPropertyEnhancements/Student.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections.Generic;
+
+namespace AutoPropertyEnhancements
+{
+    public class Student
+    {
+        public string FirstName { get; set; } = "Ivan";
+
+        public string LastName { get; set; } = "Ivanov";
+
+        public List<string> Skills { get; set; } = new List<string>()
+        {
+            "C#",
+            "JavaScript",
+            ".NET",
+            "NodeJS"
+        };
+
+        public override string ToString()
+        {
+            string strSkills = string.Join(", ", this.Skills.ToArray());
+
+            string tmpl = string.Format("Hello, my name is {0} {1} and my skills are: {2}", this.FirstName, this.LastName, strSkills);
+
+            return tmpl;
+        }
+    }
+}
