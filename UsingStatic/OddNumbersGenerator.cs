58c9d34a6715eef4c371a58a726bd3805a383d3f
csyntax <csyntax@outlook.com>
2016-05-24 11:31:15 +0300

    add using static c# 6.0 demos

---
+++ UsingStatic/OddNumbersGenerator.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.Generic;
+using static System.Linq.Enumerable;
+
+namespace UsingStatic
+{
+    public class OddNumbersGenerator
+    {
+        public IEnumerable<int> GetOddNumbersBetween(int min, int max)
+        {
+            var range = Range(min, max);
+
+            var odd = range.Where(i => i % 2 == 1);
+
+            return odd;
+        }
+    }
+}
---
+++ UsingStatic/Program.cs
@@ -0,0 +1,18 @@
+﻿using System.Linq;
+using static System.Console;
+
+namespace UsingStatic
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            OddNumbersGenerator oddNumbersGenerator = new OddNumbersGenerator();
+
+            oddNumbersGenerator
+                .GetOddNumbersBetween(1, 10)
+                .ToList()
+                .ForEach(WriteLine);
+        }
+    }
+}
